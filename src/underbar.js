225ce1e29cee04f30c8ddd392b42b4014e02e37e
John Domingo <johnwill.domingo@gmail.com>
2015-11-27 00:25:59 -0800

    Refactor _.last function using less blocky code

--- src/underbar.js
+++ src/underbar.js
@@ -38,13 +38,17 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
-    if (n === undefined) {
+    var len = array.length;
+    return n === undefined ? array[len - 1] : array.slice(len - (n > len ? len : n), len);
+
+    // VERBOSE SOLUTION //
+    /* if (n === undefined) {
       return array[array.length - 1];
     } else if (n > array.length) {
       return array;
     } else {
       return array.slice(array.length - n, array.length);
-    }
+    } */
   };
 
   // Call iterator(value, key, collection) for each element of collection.
