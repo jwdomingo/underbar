af9b7afcfa682c9aa49b275370ebc8b7383a6961
John Domingo <johnwill.domingo@gmail.com>
2015-11-29 23:07:54 -0800

    Convert arguments to array and use .apply to implement _.delay function

--- src/underbar.js
+++ src/underbar.js
@@ -377,6 +377,9 @@
   // parameter. For example _.delay(someFunction, 500, 'a', 'b') will
   // call someFunction('a', 'b') after 500ms
   _.delay = function(func, wait) {
+    var args = Array.prototype.slice.call(arguments).slice(2, arguments.length);
+
+    return args.length > 0 ? setTimeout(func.apply(func, args), wait) : setTimeout(func, wait);
   };
 
 
